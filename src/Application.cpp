258147acd851eead5aa60bc9c4d021864f0c799c
= <=>
2023-04-04 11:48:20 +0200

    Add GLEW and legacy OGL triangle

--- src/Application.cpp
+++ src/Application.cpp
@@ -1,4 +1,7 @@
+#include <iostream>
+#include <GL/glew.h>
 #include <GLFW/glfw3.h>
+
 
 int main()
 {
@@ -19,11 +22,22 @@ int main()
 	/* Make the window's context current */
 	glfwMakeContextCurrent(window);
 
+	// Initialize GLEW
+	if(glewInit() != GLEW_OK)
+		std::cout << "GLEW failed to initialize" << std::endl;
+	std::cout << glGetString(GL_VERSION) << std::endl;
+
 	/* Loop until the user closes the window */
 	while (!glfwWindowShouldClose(window))
 	{
 		/* Render here */
 		glClear(GL_COLOR_BUFFER_BIT);
+
+		glBegin(GL_TRIANGLES);
+		glVertex2f(-0.5f, -0.5f);
+		glVertex2f(0.0f, 0.5f);
+		glVertex2f(0.5f, -0.5f);
+		glEnd();
 
 		/* Swap front and back buffers */
 		glfwSwapBuffers(window);
