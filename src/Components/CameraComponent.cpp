1ef4d4706102b3f50738c9aa0e99f9a6c41f801b
Tomáš Weiss <weisstom@fit.cvut.cz>
2023-05-14 19:59:32 +0200

    Add CameraComponent

---
+++ src/Components/CameraComponent.cpp
@@ -0,0 +1,63 @@
+#include "CameraComponent.hpp"
+#include "../Entity.hpp"
+
+CameraComponent::CameraComponent(const Entity *owner, ProjectionType projectionType, float horizontalFov)
+		: _projectionType(projectionType), _horizontalFov(horizontalFov), _projectionValid(false)
+{
+	_owner = owner;
+
+	_transform = _owner->GetComponent<TransformComponent>();
+	if (_transform == nullptr)
+		throw std::runtime_error("Entity must have Transform for Camera to work");
+
+	SetWindowDimensions(1, 1);
+}
+
+void CameraComponent::SetWindowDimensions(int width, int height)
+{
+	_width = width;
+	_height = height;
+
+	_projectionValid = false;
+}
+
+const glm::mat4 &CameraComponent::View()
+{
+	return _transform->WorldToModel();
+}
+
+const glm::mat4 &CameraComponent::Projection()
+{
+	if(!_projectionValid)
+	{
+		if(_projectionType == ProjectionType::Orthographic)
+		{
+			float orthoScale = _horizontalFov / 90.0f;
+			_projectionMatrix = glm::ortho(
+					-(float)_width / 2 * orthoScale,
+					(float)_width / 2 * orthoScale,
+					-(float)_height / 2 * orthoScale,
+					(float)_height / 2 * orthoScale,
+					NearClippingPlane,
+					FarClippingPlane);
+		}
+		else
+		{
+			float aspectRatio = (float)_width / (float)_height;
+			float vFov = 2 * atan(tan(glm::radians(_horizontalFov) / 2) * aspectRatio);
+
+			_projectionMatrix = glm::perspective(
+					vFov,
+					aspectRatio,
+					NearClippingPlane,
+					FarClippingPlane);
+		}
+
+		_projectionValid = true;
+	}
+
+	return _projectionMatrix;
+}
+
+
+
---
+++ src/Components/CameraComponent.hpp
@@ -0,0 +1,44 @@
+#ifndef OGL_ENGINE_CAMERACOMPONENT_HPP
+#define OGL_ENGINE_CAMERACOMPONENT_HPP
+
+#include <glm/glm.hpp>
+#include "Component.hpp"
+#include "TransformComponent.hpp"
+
+class Entity;
+
+enum ProjectionType
+{
+	Orthographic,
+	Perspective
+};
+
+class CameraComponent : public Component
+{
+public:
+	CameraComponent(const Entity* owner, ProjectionType projectionType, float horizontalFov);
+
+	void SetWindowDimensions(int width, int height);
+
+	// Matrices
+	const glm::mat4 &View();
+	const glm::mat4 &Projection();
+
+private:
+	static constexpr float NearClippingPlane = 0.1f;
+	static constexpr float FarClippingPlane = 1000.0f;
+
+
+	// Data
+	ProjectionType _projectionType;
+	float _horizontalFov;
+	int _width, _height;
+
+	glm::mat4 _projectionMatrix;
+	bool _projectionValid;
+
+	TransformComponent* _transform;
+};
+
+
+#endif
