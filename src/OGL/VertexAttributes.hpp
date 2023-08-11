77ea0b0c7a972db1e3bfac1fb59b95533bf4e344
Tomáš Weiss <weisstom@fit.cvut.cz>
2023-08-11 14:06:33 +0200

    Add VertexAttributes for unified attribute binding

---
+++ src/OGL/VertexAttributes.hpp
@@ -0,0 +1,30 @@
+#ifndef OGL_ENGINE_VERTEXATTRIBUTES_HPP
+#define OGL_ENGINE_VERTEXATTRIBUTES_HPP
+
+#include <GL/glew.h>
+#include <stdexcept>
+
+
+enum VertexAttribute
+{
+    Position = 0,
+    Normal = 1,
+    Tangent = 2,
+    TexCoord0 = 3,
+    TexCoord1 = 4,
+    TexCoord2 = 5,
+    TexCoord3 = 6,
+    COUNT = 7
+};
+
+struct VertexAttributeInfo
+{
+    int size;
+    unsigned int type;
+    unsigned int normalized;
+    int byteSize;
+};
+
+const VertexAttributeInfo &GetVertexAttributeInfo(VertexAttribute attribute);
+
+#endif
