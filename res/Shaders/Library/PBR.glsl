74582b1a068879642b4ae439b8ad73c3e979b955
Tomáš Weiss <weisstom@fit.cvut.cz>
2024-05-10 16:37:56 +0200

    Clean up PBR

--- res/Shaders/Library/PBR.glsl
+++ res/Shaders/Library/PBR.glsl
@@ -17,29 +17,46 @@ vec3 HalfVector(vec3 v1, vec3 v2)
     return normalize(v1 + v2);
 }
 
-float Distribution_GGX(float roughness, float nDotH)
+float DistributionGGX(float roughness, float nDotH)
 {
     //                        a^4
     // D = --------------------------------------------
     //     PI * (dot(normal, half)^2 * (a^4 - 1) + 1)^2
 
-    float alpha4 = pow(roughness, 4);
-    float d = (nDotH * nDotH * (alpha4 - 1) + 1);
+    float alpha4 = pow(roughness, 4.0);
+    float d = (nDotH * nDotH * (alpha4 - 1.0) + 1.0);
     return alpha4 / (PI * d * d);
 }
 
-float Geom_Smith(float roughness, float dotProduct)
+float GeometrySchlickGGX(float roughness, float dot)
 {
-    // Shlick GGX
-    float k = (roughness + 1) * (roughness + 1 ) / 8;
-    return dotProduct / (dotProduct * (1-k) + k);
+    //     (roughness + 1)^2
+    // k = -----------------
+    //            8
+    //           n·v
+    // G = ----------------
+    //     (n·v)(1 - k) + k
+
+    float r = roughness + 1.0;
+    float k = r * r
+                /
+               8.0;
+
+    return   dot
+              /
+    (dot * (1.0 - k) + k);
 }
 
-vec3 Fresnel_Schlick(vec3 f0, float vDotH)
+float GeometrySmith(float roughness, float nDotV, float nDotL)
+{
+    return GeometrySchlickGGX(roughness, nDotV) * GeometrySchlickGGX(roughness, nDotL);
+}
+
+vec3 FresnelSchlick(vec3 f0, float vDotH)
 {
     // F = F_0 + (1 - F_0) * (1 - dot(view, half))^5
 
-    return f0 + (1 - f0) * pow(saturate(1.0 - vDotH), 5);
+    return f0 + (1.0 - f0) * pow(saturate(1.0 - vDotH), 5.0);
 }
 
 // BRDF
@@ -55,18 +72,18 @@ vec3 BRDF(Surface surface, vec3 viewDir, vec3 lightEnergy, vec3 lightDir)
     //                D * G * F
     // Spec = -------------------------
     //        4 * dot(N, L) * dot(N, V)
-    float D = Distribution_GGX(surface.roughness, nDotH);
-    float G = Geom_Smith(surface.roughness, nDotL) * Geom_Smith(surface.roughness, nDotV);
+    float D = DistributionGGX(surface.roughness, nDotH);
+    float G = GeometrySmith(surface.roughness, nDotV, nDotL);
     vec3 f0 = mix(DIELECTRIC_F0, surface.color, surface.metallic);
-    vec3 F = Fresnel_Schlick(f0, vDotH);
+    vec3 F = FresnelSchlick(f0, vDotH);
 
     vec3 SpecularBRDF =         D * G * F
                                     /
-                       4 * nDotV * nDotL + 0.0001;
+                       4.0 * nDotV * nDotL + 0.0001;
 
     // Diff
     vec3 kD = 1.0 - F;
-    vec3 lambert = mix(surface.color, vec3(0), surface.metallic);
+    vec3 lambert = surface.color * (1.0 - surface.metallic);
     vec3 DiffuseBRDF = kD * lambert * surface.occlusion / PI;
 
     return (DiffuseBRDF + SpecularBRDF) * lightEnergy * nDotL;
